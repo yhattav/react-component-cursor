8efa546770edec53ca941f0c6f8d8fc42d26e7c8
yhattav <yonatanh@wix.com>
2024-11-03 14:23:52 +0200

    same with useSmoothAnimation

--- src/CustomCursor.tsx
+++ src/CustomCursor.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
-import { useMousePosition } from './hooks/useMousePosition';
-import { Position, TargetPosition } from './types';
+import { useMousePosition, useSmoothAnimation } from './hooks';
 
 export interface CustomCursorProps {
   children?: React.ReactNode;
@@ -16,58 +15,6 @@ export interface CustomCursorProps {
 
 const ANIMATION_DURATION = '0.3s';
 const ANIMATION_NAME = 'cursorFadeIn';
-const SMOOTHING_THRESHOLD = 0.1;
-
-function useSmoothAnimation(
-  position: Position,
-  targetPosition: TargetPosition,
-  smoothFactor: number,
-  setPosition: React.Dispatch<React.SetStateAction<Position>>
-) {
-  useEffect(() => {
-    if (position.x === null || position.y === null) return;
-
-    let animationFrameId: number;
-
-    if (smoothFactor === 1) {
-      setPosition(targetPosition);
-      return;
-    }
-
-    const smoothing = () => {
-      setPosition((prev) => {
-        if (prev.x === null || prev.y === null) return prev; // TODO: Check if this is the best way to handle this. It could be that we are losing the initial call here, it could return the "next" position instead.
-
-        const dx = targetPosition.x - prev.x;
-        const dy = targetPosition.y - prev.y;
-
-        if (
-          Math.abs(dx) < SMOOTHING_THRESHOLD &&
-          Math.abs(dy) < SMOOTHING_THRESHOLD
-        ) {
-          return prev;
-        }
-
-        return {
-          x: prev.x + dx / smoothFactor,
-          y: prev.y + dy / smoothFactor,
-        };
-      });
-
-      animationFrameId = requestAnimationFrame(smoothing);
-    };
-
-    animationFrameId = requestAnimationFrame(smoothing);
-
-    return () => {
-      if (animationFrameId) {
-        cancelAnimationFrame(animationFrameId);
-      }
-    };
-  }, [position.x, position.y, targetPosition, smoothFactor, setPosition]);
-
-  return null;
-}
 
 export const CustomCursor: React.FC<CustomCursorProps> = ({
   children,
